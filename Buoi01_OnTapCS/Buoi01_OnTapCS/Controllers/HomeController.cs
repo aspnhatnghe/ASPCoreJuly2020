807a4d82a68869e3847f493e502a656448e57fbc
NN <hyhien@gmail.com>
2020-07-11 20:53:09 +0700

    Done Buoi01

---
+++ Buoi01_OnTapCS/Buoi01_OnTapCS/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Buoi01_OnTapCS.Models;
+
+namespace Buoi01_OnTapCS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Demo()
+        {
+            //URL: /Home/Demo
+            ViewBag.ViewName = "Demo";
+            return View();
+        }
+
+        public IActionResult Demo2()
+        {
+            //URL: /Home/Demo2
+            ViewBag.ViewName = "Demo2";
+            return View("Demo");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
