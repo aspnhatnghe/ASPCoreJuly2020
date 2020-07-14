cbc7e75bc18a16fd9fa001c13bc3030249990bea
NN <hyhien@gmail.com>
2020-07-14 20:54:57 +0700

    Buoi02: HTML + CSS

---
+++ Buoi02_HTMLCSS/Buoi02_HTMLCSS/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Buoi02_HTMLCSS.Models;
+
+namespace Buoi02_HTMLCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessDemo(int GioiTinh, string[] NgoaiNgu, string QueQuan, string HoTen)
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Buoi02_HTMLCSS/Buoi02_HTMLCSS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi02_HTMLCSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
