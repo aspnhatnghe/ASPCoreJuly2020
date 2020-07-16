90b7999b93db94e58826847f656dd6829d6c2137
NN <hyhien@gmail.com>
2020-07-16 20:58:44 +0700

    Done Day 03 - CSS

---
+++ Buoi03_CSS/Buoi03_CSS/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Buoi03_CSS.Models;
+
+namespace Buoi03_CSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult SanPham()
+        {
+            Random rd = new Random();
+            int soLuong = rd.Next(4, 100);
+            List<HangHoa> dsHangHoa = new List<HangHoa>();
+
+            for (int i = 0; i < soLuong; i++)
+            {
+                dsHangHoa.Add(new HangHoa { 
+                    TenHH = $"Samsung {rd.Next()}",
+                    DonGia = rd.Next(1000, 10000),
+                    GiamGia = rd.NextDouble()
+                });
+            }
+
+            return View(dsHangHoa);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Buoi03_CSS/Buoi03_CSS/Models/HangHoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi03_CSS.Models
+{
+    public class HangHoa
+    {
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public double GiamGia { get; set; }
+
+        public double GiaBan => DonGia * (1 - GiamGia);
+        public bool DangGiamGia => GiamGia > 0;
+    }
+}
