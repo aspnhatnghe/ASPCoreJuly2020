9750749c3d608ba4d8d76ad5c294da11642c74cf
NN <hyhien@gmail.com>
2020-08-01 19:14:32 +0700

    Upload single files

--- Buoi09_Validation/Buoi09_Validation/Controllers/DemoController.cs
+++ Buoi09_Validation/Buoi09_Validation/Controllers/DemoController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Buoi09_Validation.Controllers
 {
@@ -7,6 +10,53 @@ namespace Buoi09_Validation.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        private void MoveUploadedFile(IFormFile myfile, List<string> folder)
+        {
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory());
+            for (int i = 0; i < folder.Count; i++)
+            {
+                fullPath = Path.Combine(fullPath, folder[i]);
+            }
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+
+            fullPath = Path.Combine(fullPath, Path.GetFileName(myfile.FileName));
+
+            using (var file = new FileStream(fullPath, FileMode.Create))
+            {
+                myfile.CopyTo(file);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SingleFile(IFormFile myfile)
+        {
+            MoveUploadedFile(myfile, new List<string> { "wwwroot", "SingleFiles" });
+            /*
+            var uploadedFileName = Path.GetFileName(myfile.FileName);
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Data", uploadedFileName);
+
+            if (myfile != null && myfile.Length > 0)
+            {
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    myfile.CopyTo(file);
+                }
+            }
+            */
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult MultipleFile(List<IFormFile> myfile)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
