ba155972d8b27ceb5108e44dd7074b398027a969
NN <hyhien@gmail.com>
2020-08-01 18:11:47 +0700

    Buoi09 - Init

---
+++ Buoi09_Validation/Buoi09_Validation/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi09_Validation.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
