7debf4764a0bfb3e3487767958eb819d66a75c3e
NN <hyhien@gmail.com>
2020-08-04 19:12:45 +0700

    Done Regular Expression

--- Buoi10_Validation_Layout/Buoi10_Validation_Layout/Models/Employee.cs
+++ Buoi10_Validation_Layout/Buoi10_Validation_Layout/Models/Employee.cs
@@ -13,6 +13,11 @@ namespace Buoi10_Validation_Layout.Models
         [Required(ErrorMessage = "*")]
         [Remote(action: "CheckEmployeeExist", controller: "Employee", ErrorMessage = "Mã này đã tồn tại")]
         public string EmployeeNo { get; set; }
+
+        [RegularExpression("^0[5789][0-9]{8}$")]
         public string Phone { get; set; }
+
+        [RegularExpression(@"^\w+([.+-]\w+)*@\w{2,}([.]\w{2,4})+$", ErrorMessage ="Chưa đúng định dạng email")]
+        public string Email { get; set; }
     }
 }
