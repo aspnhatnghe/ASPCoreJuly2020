c68920caceb9dd950e28fe2bcd5b4572485a3c63
NN <hyhien@gmail.com>
2020-08-04 18:40:18 +0700

    Add remote validation attribute

---
+++ Buoi10_Validation_Layout/Buoi10_Validation_Layout/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi10_Validation_Layout.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi10_Validation_Layout.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee employee)
+        {
+            return View();
+        }
+
+        public IActionResult CheckEmployeeExist(string EmployeeNo)
+        {
+            var accounts = new List<string>()
+            {
+                "admin", "guest", "abc"
+            };
+
+            if(accounts.Contains(EmployeeNo))
+            {
+                return Json("Mã này đã có");
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ Buoi10_Validation_Layout/Buoi10_Validation_Layout/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi10_Validation_Layout.Models
+{
+    public class Employee
+    {
+        [Display(Name ="Mã nhân viên")]
+        [Required(ErrorMessage = "*")]
+        [Remote(action: "CheckEmployeeExist", controller: "Employee", ErrorMessage = "Mã này đã tồn tại")]
+        public string EmployeeNo { get; set; }
+        public string Phone { get; set; }
+    }
+}
