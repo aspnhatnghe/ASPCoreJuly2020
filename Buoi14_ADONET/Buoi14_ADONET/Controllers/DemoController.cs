84338186ffcfe547ec67a2bbe9a9a5f6ac42f4d2
NN <hyhien@gmail.com>
2020-08-13 19:08:55 +0700

    ADO.NET Basic

---
+++ Buoi14_ADONET/Buoi14_ADONET/Controllers/DemoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Buoi14_ADONET.Controllers
+{
+    public class DemoController : Controller
+    {
+        string chuoiKetNoi = @"Data Source=.;Initial Catalog=eStore20;Integrated Security=True";
+        public IActionResult GetData()
+        {
+            SqlConnection connection = new SqlConnection(chuoiKetNoi);
+
+            var sql = "SELECT MaHH, TenHH, DonGia FROM HangHoa";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, connection);
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            //Xử lý kết quả
+            var sb = new StringBuilder();
+            foreach(DataRow dr in dataTable.Rows)
+            {
+                sb.AppendLine($"{dr["MaHH"]} - {dr["TenHH"]} - {dr["DonGia"]}");
+            }
+
+            return Content(sb.ToString());
+            //return View();
+        }
+
+        public IActionResult ChangeData()
+        {
+            SqlConnection connection = new SqlConnection(chuoiKetNoi);
+
+            var sql = "INSERT INTO Loai(TenLoai, MoTa, Hinh) VALUES(N'Nước giải khát', N'Uống có hại nhưng lợi', NULL)";
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+
+            return Content($"{result}");
+        }
+    }
+}
---
+++ Buoi14_ADONET/Buoi14_ADONET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi14_ADONET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
