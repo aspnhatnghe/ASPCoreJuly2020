b782d81fe3c933a6d8eb2f0033c1fedd4b197873
NN <hyhien@gmail.com>
2020-08-15 19:28:24 +0700

    Drapper ORM

---
+++ Buoi14_ADONET/Buoi14_ADONET/Controllers/QLLoaiController.cs
@@ -0,0 +1,62 @@
+﻿using Buoi14_ADONET.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Ini;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi14_ADONET.Controllers
+{
+    public class QLLoaiController : Controller
+    {
+        private readonly string chuoiKetNoi;
+        public QLLoaiController(IConfiguration _config)
+        {
+            chuoiKetNoi = _config.GetConnectionString("Database1");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (var con = new SqlConnection(chuoiKetNoi))
+            {
+                var dsLoai = await con.QueryAsync<Loai>("SELECT * FROM Loai ORDER BY TenLoai");
+
+                return View(dsLoai.ToList());
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            using (var con = new SqlConnection(chuoiKetNoi))
+            {
+                var loai = await con.QueryFirstAsync<Loai>("SELECT * FROM Loai WHERE MaLoai = @Ma", new { Ma = id });
+
+                return View(loai);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Loai loai)
+        {
+            using (var con = new SqlConnection(chuoiKetNoi))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("MaLoai", loai.MaLoai);
+                parameters.Add("TenLoai", loai.TenLoai);
+                parameters.Add("MoTa", loai.MoTa);
+                parameters.Add("Hinh", loai.Hinh);
+
+                int rowEffect = con.Execute("spSuaLoai", parameters, commandType: CommandType.StoredProcedure);
+                if(rowEffect > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Edit", new { id = loai.MaLoai});
+                //return Edit(loai.MaLoai);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ﻿# ASPCoreJuly2020
 
 # Khai giảng 11/07/2020
+
+# ORM Drapper
+* Cài Drapper
+## Select
+* Lấy nhiều:
+var allEvents = connection.Query<Loai>("SELECT MaLoai, TenLoai FROM Event");
+connection.Query<T>(sql [,parameters])
+
+* Insert/Update/Delete
+connection.Execute(sql [,parameters])
