68523b00e8f08a386e8cfcbfb45d5a0a64ffefbf
NN <hyhien@gmail.com>
2020-08-22 20:38:46 +0700

    ChartJs

---
+++ MyStoreDbFirst/Controllers/ThongKeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStoreDbFirst.Entities;
+using MyStoreDbFirst.ViewModels;
+using System.Linq;
+
+namespace MyStoreDbFirst.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        private readonly eStore20Context _context;
+
+        public ThongKeController(eStore20Context ctx)
+        {
+            _context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.ChiTietHd
+                .GroupBy(cthd => cthd.MaHhNavigation.MaLoaiNavigation.TenLoai)
+                .Select(g => new ThongKeViewModel
+                {
+                    Loai = g.Key,
+                    DoanhThu = g.Sum(cthd => cthd.SoLuong * cthd.DonGia * (1 - cthd.GiamGia)),
+                    GiaNN = g.Min(cthd => cthd.DonGia),
+                    GiaTB = g.Average(cthd => cthd.DonGia),
+                    SoMatHangDaBan = g.Sum(cthd => cthd.SoLuong)
+                });
+
+            return Json(data.ToList());
+            //return View();
+        }
+
+        public IActionResult DoanhThuKhachHang()
+        {
+            var data = _context.ChiTietHd
+                .GroupBy(cthd => new
+                {
+                    MaKh = cthd.MaHdNavigation.MaKh,
+                    HoTen = cthd.MaHdNavigation.MaKhNavigation.HoTen,
+                    Thang = cthd.MaHdNavigation.NgayDat.Month,
+                    Nam = cthd.MaHdNavigation.NgayDat.Year
+                })
+                .Select(g => new { 
+                    MaKh = g.Key.MaKh,
+                    HoTen = g.Key.HoTen,
+                    NamThang = $"{g.Key.Thang}/{g.Key.Nam}",
+                    DoanhThu = g.Sum(cthd => cthd.SoLuong * cthd.DonGia * (1 - cthd.GiamGia))
+                });
+
+            return Json(data.ToList());
+        }
+
+        public IActionResult DoanhThuThang()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyStoreDbFirst/ViewModels/ThongKeViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MyStoreDbFirst.ViewModels
+{
+    public class ThongKeViewModel
+    {
+        public string Loai { get; set; }
+        public double DoanhThu { get; set; }
+        public double GiaNN { get; set; }
+        public double GiaTB { get; set; }
+        public int SoMatHangDaBan { get; set; }
+    }
+}
