658f6579a6530f58a0203da9bb372776ec0b97c1
NN <hyhien@gmail.com>
2020-09-01 19:49:33 +0700

    API example

---
+++ MyStoreDbFirst/Controllers/DemoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyStoreDbFirst.Entities;
+
+namespace MyStoreDbFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public static List<Loai> DanhSach { get; set; } = new List<Loai>();
+        // GET: api/Demo
+        [HttpGet]
+        public IEnumerable<Loai> Get()
+        {
+            return DanhSach;
+        }
+
+        // GET: api/Demo/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var loai = DanhSach.FirstOrDefault(lo => lo.MaLoai == id);
+            if (loai != null)
+                return Ok(loai);
+            return NotFound();
+        }
+
+        // POST: api/Demo
+        [HttpPost]
+        public IActionResult Post([FromBody] Loai loai)
+        {
+            var lo = DanhSach.SingleOrDefault(lo => lo.MaLoai == loai.MaLoai);
+            if (lo != null) return BadRequest();
+            DanhSach.Add(loai);
+            return Ok();
+        }
+
+        // PUT: api/Demo/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Loai loai)
+        {
+            if (id != loai.MaLoai)
+                return BadRequest();
+            var lo = DanhSach.SingleOrDefault(lo => lo.MaLoai == id);
+            if (lo == null)
+                return BadRequest();
+
+            lo.TenLoai = loai.TenLoai;
+            lo.MoTa = loai.MoTa;
+            lo.Hinh = loai.Hinh;
+            return Ok();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var lo = DanhSach.SingleOrDefault(lo => lo.MaLoai == id);
+            if (lo == null)
+                return BadRequest();
+            DanhSach.Remove(lo);
+            return Ok();
+        }
+    }
+}
---
+++ MyStoreDbFirst/Controllers/KhachHangController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyStoreDbFirst.Entities;
+
+namespace MyStoreDbFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhachHangController : ControllerBase
+    {
+        private readonly eStore20Context _context;
+
+        public KhachHangController(eStore20Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/KhachHang
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KhachHang>>> GetKhachHang()
+        {
+            return await _context.KhachHang.ToListAsync();
+        }
+
+        // GET: api/KhachHang/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KhachHang>> GetKhachHang(string id)
+        {
+            var khachHang = await _context.KhachHang.FindAsync(id);
+
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return khachHang;
+        }
+
+        // PUT: api/KhachHang/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKhachHang(string id, KhachHang khachHang)
+        {
+            if (id != khachHang.MaKh)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(khachHang).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!KhachHangExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/KhachHang
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<KhachHang>> PostKhachHang(KhachHang khachHang)
+        {
+            _context.KhachHang.Add(khachHang);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (KhachHangExists(khachHang.MaKh))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetKhachHang", new { id = khachHang.MaKh }, khachHang);
+        }
+
+        // DELETE: api/KhachHang/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<KhachHang>> DeleteKhachHang(string id)
+        {
+            var khachHang = await _context.KhachHang.FindAsync(id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            _context.KhachHang.Remove(khachHang);
+            await _context.SaveChangesAsync();
+
+            return khachHang;
+        }
+
+        private bool KhachHangExists(string id)
+        {
+            return _context.KhachHang.Any(e => e.MaKh == id);
+        }
+    }
+}
